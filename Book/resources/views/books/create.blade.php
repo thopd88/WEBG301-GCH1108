c956a4ed2abc41632d470f70204d9bb6db7fe228
Tho Pham (Alex) <20041501+thopd88@users.noreply.github.com>
2023-10-13 14:28:22 +0700

    using blade layouts

--- Book/resources/views/books/create.blade.php
+++ Book/resources/views/books/create.blade.php
@@ -1,69 +1,38 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-  <title>Title</title>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- Bootstrap CSS v5.2.1 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-
-</head>
-
-<body>
-  <header>
-    <!-- place navbar here -->
-  </header>
-  <main>
-    <form action="/books" method="post">
-        @csrf
-        <div class="mb-3">
-          <label for="" class="form-label">Title</label>
-          <input type="text"
-            class="form-control" name="title" id="title" aria-describedby="helpId" placeholder="">
-        </div>
-        <div class="mb-3">
-          <label for="" class="form-label">Author</label>
-          <input type="text"
-            class="form-control" name="author" id="author" aria-describedby="helpId" placeholder="">
-        </div>
-        <div class="mb-3">
-          <label for="category_id" class="form-label">Category</label>
-          <select class="form-select form-select-lg" name="category_id" id="category_id">
-            @foreach ($categories as $category)
-              <option value="{{ $category->id }}">{{ $category->name }}</option>
-            @endforeach
-          </select>
-        </div>
-        <div class="mb-3">
-          <label for="tags" class="form-label">Tags</label>
-          <select class="form-select form-select-lg" name="tags[]" id="tags" multiple>
-            @foreach ($tags as $tag)
-              <option value="{{ $tag->id }}">{{ $tag->name }}</option>
-            @endforeach
-          </select>
-        </div>
-        <div class="mb-3">
-          <label for="" class="form-label">Description</label>
-          <textarea class="form-control" name="description" id="description" rows="3"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-  </main>
-  <footer>
-    <!-- place footer here -->
-  </footer>
-  <!-- Bootstrap JavaScript Libraries -->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-  </script>
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
-    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
-  </script>
-</body>
-
-</html>
+@extends('layouts.app')
+@section('title', 'Books Create')
+@section('content')
+<form action="/books" method="post">
+  @csrf
+  <div class="mb-3">
+    <label for="" class="form-label">Title</label>
+    <input type="text"
+      class="form-control" name="title" id="title" aria-describedby="helpId" placeholder="">
+  </div>
+  <div class="mb-3">
+    <label for="" class="form-label">Author</label>
+    <input type="text"
+      class="form-control" name="author" id="author" aria-describedby="helpId" placeholder="">
+  </div>
+  <div class="mb-3">
+    <label for="category_id" class="form-label">Category</label>
+    <select class="form-select form-select-lg" name="category_id" id="category_id">
+      @foreach ($categories as $category)
+        <option value="{{ $category->id }}">{{ $category->name }}</option>
+      @endforeach
+    </select>
+  </div>
+  <div class="mb-3">
+    <label for="tags" class="form-label">Tags</label>
+    <select class="form-select form-select-lg" name="tags[]" id="tags" multiple>
+      @foreach ($tags as $tag)
+        <option value="{{ $tag->id }}">{{ $tag->name }}</option>
+      @endforeach
+    </select>
+  </div>
+  <div class="mb-3">
+    <label for="" class="form-label">Description</label>
+    <textarea class="form-control" name="description" id="description" rows="3"></textarea>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+@endsection
--- Book/resources/views/books/index.blade.php
+++ Book/resources/views/books/index.blade.php
@@ -1,108 +1,45 @@
-{{-- Show a simple table with all books --}}
-<!doctype html>
-<html lang="en">
-
-<head>
-  <title>Title</title>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- Bootstrap CSS v5.2.1 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-
-</head>
-
-<body>
-  <header>
-    <nav class="navbar navbar-expand-sm navbar-light bg-light">
-        <div class="container">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-          aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="collapsibleNavId">
-          <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-              <div class="dropdown-menu" aria-labelledby="dropdownId">
-                <a class="dropdown-item" href="#">Action 1</a>
-                <a class="dropdown-item" href="#">Action 2</a>
-              </div>
-            </li>
-          </ul>
-          <form class="d-flex my-2 my-lg-0" method="POST" action="/search">
-            @csrf
-              <input class="form-control me-sm-2" type="text" placeholder="Search" id="search" name="search">
-              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-      </div>
-    </nav>
-    
-  </header>
-  <main>
-    <div class="table-responsive">
-        <table class="table table-primary">
-            <thead>
-                <tr>
-                    <th scope="col">Title</th>
-                    <th scope="col">Author</th>
-                    <th scope="col">Description</th>
-                    <th>Category</th>
-                    <th>Tags</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($books as $book)
-                    <tr>
-                        <td>
-                            <a href="/books/{{$book->id}}">{{$book->title}}</a>
-                        </td>
-                        <td>{{ $book->author }}</td>
-                        <td>{{ $book->description }}</td>
-                        <td>{{ $book->category->name }}</td>
-                        <td>
-                            @foreach ($book->tags as $tag)
-                                <span class="badge bg-primary">{{ $tag->name }}</span>
-                            @endforeach
-                        </td>
-                        <td>
-                            <a href="/books/{{$book->id}}/edit">
-                                <button type="button" class="btn btn-primary">Edit</button>
-                            </a>
-                            <form action="/books/{{$book->id}}" method="post">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?');">Delete</button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-  </main>
-  <footer>
-    <!-- place footer here -->
-  </footer>
-  <!-- Bootstrap JavaScript Libraries -->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-  </script>
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
-    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
-  </script>
-</body>
-
-</html>
+@extends('layouts.app')
+@section('title', 'Books Index')
+@section('content')
+<div class="table-responsive">
+  <table class="table table-primary">
+      <thead>
+          <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Author</th>
+              <th scope="col">Description</th>
+              <th>Category</th>
+              <th>Tags</th>
+              <th>Action</th>
+          </tr>
+      </thead>
+      <tbody>
+          @foreach ($books as $book)
+              <tr>
+                  <td>
+                      <a href="/books/{{$book->id}}">{{$book->title}}</a>
+                  </td>
+                  <td>{{ $book->author }}</td>
+                  <td>{{ $book->description }}</td>
+                  <td>{{ $book->category->name }}</td>
+                  <td>
+                      @foreach ($book->tags as $tag)
+                          <span class="badge bg-primary">{{ $tag->name }}</span>
+                      @endforeach
+                  </td>
+                  <td>
+                      <a href="/books/{{$book->id}}/edit">
+                          <button type="button" class="btn btn-primary">Edit</button>
+                      </a>
+                      <form action="/books/{{$book->id}}" method="post">
+                          @csrf
+                          @method('DELETE')
+                          <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?');">Delete</button>
+                      </form>
+                  </td>
+              </tr>
+          @endforeach
+      </tbody>
+  </table>
+</div>
+@endsection
